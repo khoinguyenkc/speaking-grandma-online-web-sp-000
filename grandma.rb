011b8df2db177b917f3d9904fa24adac8a688da5
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-13 20:13:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,8 @@
 def speak_to_grandma(phrase)
 
   if phrase == phrase.upcase
-    #lowercase
-    return "?! SPEAK UP, SONNY!"
+    #aka lowercase
+    return "HUH?! SPEAK UP, SONNY!"
   else
     if phrase == "I LOVE YOU GRANDMA!'"
       return "I LOVE YOU TOO PUMPKIN!"
